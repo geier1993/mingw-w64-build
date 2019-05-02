97ae95b31ead3d60c5114a637e2bbc214fda45ec
Philipp Geier <Philipp.geier@krauth-online.de>
2019-05-02 10:19:45 +0200

    Added nix-shell

---
+++ default.nix
@@ -0,0 +1,17 @@
+with import <nixpkgs> {};
+#stdenv.mkDerivation rec {
+#NoCC important to overwrite compiler..
+stdenvNoCC.mkDerivation rec {
+  name = "mingw";
+  env = buildEnv { name = name; paths = buildInputs; };
+  hardeningDisable = [ "format" ];  # to build the cross-compiler, otherwise  there ar erros with -Werror=format-security
+  buildInputs = [
+    gcc       # make sure gcc is used to link the C preprocessor cpp properly
+    bison
+    flex
+    gmp       # required for gcc
+    mpfr      # required for gcc
+    libmpc    # required for gcc
+    isl
+  ];
+}
